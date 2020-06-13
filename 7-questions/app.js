73ae364bbe552bdeced42af081e3b04593d823a6
Traian <niculescu.traian.ct@gmail.com>
2020-06-13 15:31:34 +0300

    update

--- 7-questions/app.js
+++ 7-questions/app.js
@@ -1,2 +1,14 @@
-//using selectors inside the element
-// traversing the dom
+// 1. traversing the dom
+const btns = document.querySelectorAll('.question-btn');
+//  add event listeners to every button
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');
+  });
+});
+
+// 2. using selectors inside the element
+
+// target is the element that triggered the event (e.g., the user clicked on)
+// currentTarget is the element that the event listener is attached to.
