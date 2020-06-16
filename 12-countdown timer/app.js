9981fd4c0af53e84e45d601129796a6a46bed1d5
Traian <niculescu.traian.ct@gmail.com>
2020-06-16 23:51:55 +0300

    countdown timer

--- 12-countdown timer/app.js
+++ 12-countdown timer/app.js
@@ -1,26 +1,139 @@
 const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
+  'Ianuarie',
+  'Februarie',
+  'Martie',
+  'Aprilie',
+  'Mai',
+  'Iunie',
+  'Iulie',
   'August',
-  'September',
-  'October',
-  'November',
-  'December'
+  'Septembrie',
+  'Octombrie',
+  'Noiembrie',
+  'Decembrie'
 ];
 const weekdays = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday'
+  'Duminica',
+  'Luni',
+  'Marti',
+  'Miercuri',
+  'Joi',
+  'vineri',
+  'Sambata'
 ];
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
+
+const maxDays = 3; // insert here the maximum of random days for the promo to be valid
+const randomPromoDays = Math.ceil(Math.random() * maxDays);
+// console.log(randomPromoDays); -> a day between 1 and 3
+
+const setFutureDate = () => {
+  let futureDate;
+  if (!localStorage.getItem('futureDate')) {
+    futureDate = new Date(Date.now() + 8.64e7 * randomPromoDays);
+    localStorage.setItem('futureDate', futureDate);
+  } else {
+    futureDate = new Date(localStorage.getItem('futureDate'));
+    // if the localStorage time - now < 2h then re-assign futureDate here and in the localStorage
+    if (futureDate.getTime() - Date.now() < 72e5) {
+      futureDate = new Date(Date.now() + 8.64e7 * randomPromoDays);
+      localStorage.setItem('futureDate', futureDate);
+    }
+  }
+  return futureDate;
+};
+
+// console.log(
+//   `future: ${futureDate}, diff in hours: ${(
+//     (futureDate.getTime() - now.getTime()) /
+//     36e5
+//   ).toFixed(1)}`
+// );
+
+let futureDate = setFutureDate();
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `Promotia se termina ${weekday}, ${date} ${month} ${year}, ${
+  hours < 10 ? '0' + hours : hours
+}:${minutes < 10 ? '0' + minutes : minutes} ${(hours > 12
+  ? 'pm'
+  : 'am'
+).toUpperCase()}`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+// console.log('future time:', futureTime);
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  // console.log('today:', today);
+  const t = futureTime - today; // -> the diff between future time and now
+  // console.log(t);
+
+  // values in ms
+  const oneDay = 8.64e7;
+  const oneHour = 3.6e6;
+  const oneMinute = 6e4;
+
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  // console.log(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  // console.log(hours);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+  return {
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+};
+
+const displayRemainingTime = (remainingTime) => {
+  const daysUI = document.querySelector('.days');
+  const hoursUI = document.querySelector('.hours');
+  const minutesUI = document.querySelector('.minutes');
+  const secondsUI = document.querySelector('.seconds');
+
+  const daysSpanUI = document.querySelector('.days + span');
+  const hoursSpanUI = document.querySelector('.hours + span');
+  const minutesSpanUI = document.querySelector('.minutes + span');
+  const secondsSpanUI = document.querySelector('.seconds + span');
+
+  daysUI.textContent = remainingTime.days;
+  hoursUI.textContent =
+    remainingTime.hours < 10 ? '0' + remainingTime.hours : remainingTime.hours;
+  minutesUI.textContent =
+    remainingTime.minutes < 10
+      ? '0' + remainingTime.minutes
+      : remainingTime.minutes;
+  secondsUI.textContent =
+    remainingTime.seconds < 10
+      ? '0' + remainingTime.seconds
+      : remainingTime.seconds;
+
+  daysSpanUI.textContent = remainingTime.days === 1 ? 'zi' : 'zile';
+  hoursSpanUI.textContent = remainingTime.hours === 1 ? 'ora' : 'ore';
+  minutesSpanUI.textContent = remainingTime.minutes === 1 ? 'minut' : 'minute';
+  secondsSpanUI.textContent = 'sec';
+};
+
+window.addEventListener(
+  'DOMContentLoaded',
+  displayRemainingTime(getRemainingTime())
+);
+
+setInterval(() => {
+  displayRemainingTime(getRemainingTime());
+}, 1000);
